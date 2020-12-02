d6a3b7fcd18611faa1d6d496b11716c915a6b903
Leobroud <leo.oliveira.b@gmail.com>
2020-12-02 16:21:53 -0300

    função da tela de login criada

--- front-end/src/screens/login/Login.js
+++ front-end/src/screens/login/Login.js
@@ -1,4 +1,36 @@
+import React from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom';
+
 const Login = () => {
+  const history = useHistory()
+
+  const {form, onChange, resetState}  = useForm({
+    nickname:"",
+    password:""
+  })
+
+  const handleInputChange = (event) =>{
+    const { name, value } = event.target
+    onChange(name, value)
+  }
+  
+  const handleSubmittion = (event) =>{
+    event.preventDefault()
+    handleLogin()
+    resetState()
+  }
+
+  const handleLogin = () => {
+    axios
+      .post("Colocar a URL", form)
+      .then(response => {
+        history.push("/view")
+      }).catch(error => {
+        console.log(error)
+      })
+  }
+
   return (
     <div>Login</div>
   )
