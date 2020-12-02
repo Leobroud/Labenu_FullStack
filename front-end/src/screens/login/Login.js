d7650bcbd161ff27f745474385607c34c4f649de
Leobroud <leo.oliveira.b@gmail.com>
2020-12-02 18:40:40 -0300

    editando tela de login

--- front-end/src/screens/login/Login.js
+++ front-end/src/screens/login/Login.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import axios from 'axios'
 import { useHistory } from 'react-router-dom';
+import useForm from '../../hooks/useForm';
+import { Input, Stack, Container, Box } from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react"
 
 const Login = () => {
   const history = useHistory()
 
   const {form, onChange, resetState}  = useForm({
-    nickname:"",
+    email:"",
     password:""
   })
 
@@ -32,7 +35,22 @@ const Login = () => {
   }
 
   return (
-    <div>Login</div>
+    <>
+      <Button size="sm" margin="10px">Criar Conta</Button>
+      <Container maxW="xl" centerContent>
+        <Box  padding="4"  maxW="3x1" marginTop='100px' borderWidth="1px" borderRadius="lg" overflow="hidden">
+          <form onSubmit={handleSubmittion} autoComplete="off" >
+            <Stack spacing={4} w="300px">
+              <Input autoComplete="off" name="email" onChange={handleInputChange} type="email" placeholder="E-mail" size="md" />
+              <Input name="password" onChange={handleInputChange} type="password" placeholder="Digite a Senha" size="md" />
+              <Button type="submit" size="md" onClick={handleLogin}>Entrar</Button>
+              <Button size="sm" variant="link">Crie uma conta</Button>
+            </Stack>
+          </form>
+        </Box>
+      </Container>  
+    </>
+
   )
 }
 
