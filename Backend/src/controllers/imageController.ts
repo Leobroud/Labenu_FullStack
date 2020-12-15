d1565c815657e2aff611d88b5a005e25dc24ed07
Leobroud <leo.oliveira.b@gmail.com>
2020-12-14 23:28:25 -0300

    correção de bugs

--- Backend/src/controllers/imageController.ts
+++ Backend/src/controllers/imageController.ts
@@ -8,12 +8,38 @@ class imageController {
   static async index(req: Request, res: Response) {
 
     try { 
-      const image = await knex('IMAGES').select('*')
 
-      return res.status(200).json(image)
+      let message = "Success!"
+
+      const queryResult: any = await knex('IMAGES')
+        .select("*")
+
+        if (!queryResult[0]) {
+          res.statusCode = 404
+          message = "Image not found"
+          throw new Error(message)
+        }
+  
+        const image: Image = {
+          id: queryResult[0].id,
+          subtitle: queryResult[0].subtitle,
+          author: queryResult[0].author,
+          date: queryResult[0].date,
+          file: queryResult[0].file,
+          tags: queryResult[0].tags,
+          collection: queryResult[0].collection,
+          userId: queryResult[0].user_id,
+        }
+  
+        res.status(200).send({ message, image })
 
     }catch(error) {
-      return res.status(500).send(error)
+      
+      let message = error.sqlMessage || error.message
+      res.statusCode = 400
+
+      res.send({ message })
+
     }
   }
 
--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -5,10 +5,10 @@ import UserController from './controllers/userController'
 const routes = Router()
 
 
-routes.post('/user', UserController.login)
+routes.post('/user/login', UserController.login)
 routes.post('/user/signup', UserController.create)
 routes.post('/image/create', imageController.create)
 routes.get('/image/:id', imageController.show)
-routes.get('/image', imageController.index)
+routes.get('/image/all', imageController.index)
 
 export default routes
