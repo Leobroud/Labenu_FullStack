d578ed8bbd92538a8491dc4cb8fe39e242436661
Leobroud <leo.oliveira.b@gmail.com>
2020-12-17 16:04:22 -0300

    alterando server

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './routes'
+import bodyParser from 'body-parser'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.json({limit: "500mb"}))
+app.use(bodyParser.urlencoded({limit: '500mb', extended: true}))
 app.use(routes)
+
 
 app.listen(3003, ()=>{
     console.log("Server listening on http://localhost:3003")
